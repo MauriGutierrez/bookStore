30abb5682b38b47c1b99645fa3607bcd9d85edd2
Mauricio Gutierrez <maurix554@gmail.com>
2021-08-20 11:27:24 -0300

    Added CONFIGURATION MODULE

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from './config/config.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot()],
+  imports: [TypeOrmModule.forRoot(), ConfigModule],
   controllers: [AppController],
   providers: [AppService],
 })
