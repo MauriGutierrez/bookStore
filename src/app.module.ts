61c7ea128f450416977f9ac9e1e454c3185a3a8c
Mauricio Gutierrez <maurix554@gmail.com>
2021-08-20 12:31:44 -0300

    Added congig.keys

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from './config/config.module';
+import { ConfigService } from './config/config.service';
+import { Configuration } from './config/config.keys';
 
 @Module({
   imports: [TypeOrmModule.forRoot(), ConfigModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+
+  static port: number | string;
+  constructor(private readonly _configService: ConfigService) {
+    AppModule.port= this._configService.get(Configuration.PORT)
+  }
+}
